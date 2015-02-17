92b6360dd63d2ee9862c8412d09c32b68d72e4ac
Jesus Fernandez <jsfdez@gmail.com>
2015-02-17 08:50:08 +0100

    Modifying pixels

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,8 @@
 #include "game.h"
 
+#include <cstdint>
+
+#include "SDL.h"
 #include "files.h"
 #include "imageloader.h"
 
@@ -13,7 +16,7 @@ Game::~Game()
 	Destroy();
 }
 
-Game::Status Game::Update(const SDL_Event&, SDL_Surface& surface)
+Game::Status Game::Update(const SDL_Event& event, SDL_Surface& surface)
 {
 	const auto offsetX = 330u;
 	const auto offsetY = 100u;
@@ -27,7 +30,33 @@ Game::Status Game::Update(const SDL_Event&, SDL_Surface& surface)
 		SDL_Rect rect{ offsetX + w * (i % columns), 
 			offsetY + h * (i / columns), w, h };
 		auto image = m_gems[m_gridData[i]];
-		SDL_BlitSurface(image.get(), nullptr, &surface, &rect);
+		if (event.type == SDL_MOUSEBUTTONDOWN)
+		{
+			std::unique_ptr<SDL_Surface, decltype(SDL_FreeSurface)*> copy(
+				nullptr, SDL_FreeSurface);
+			copy.reset(SDL_CreateRGBSurface(0, image->w, image->h, 
+				image->format->BitsPerPixel, 
+				image->format->Rmask, 
+				image->format->Gmask, 
+				image->format->Bmask, 
+				image->format->Amask));
+			SDL_BlitSurface(image.get(), nullptr, copy.get(), nullptr);
+			if (copy)
+			{
+				auto size = copy->w * copy->h;
+				for (decltype(size) i = 0; i < size; i += 4)
+				{
+					auto pixel = static_cast<std::uint32_t*>(copy->pixels)[i];
+					auto rgba = reinterpret_cast<std::uint8_t*>(&pixel);
+					//rgba[0] = 0xFF;
+				}
+			}
+			SDL_BlitSurface(copy.get(), nullptr, &surface, &rect);
+		}
+		else
+		{
+			SDL_BlitSurface(image.get(), nullptr, &surface, &rect);
+		}
 	}
     return status;
 }
