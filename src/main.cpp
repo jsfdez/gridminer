57a46cc956e383897fc25bb8ea836dc5eccad800
Jesus Fernandez <jsfdez@gmail.com>
2015-02-20 08:29:22 +0100

    FPS limit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <stack>
 #include <memory>
+#include <chrono>
+#include <cstdint>
 #include <stdio.h>
 #include <cstdlib>
 
@@ -40,6 +42,7 @@ int main(int, char**)
 	scenes.emplace(new GameSurface);
     while(true)
 	{
+		auto startTime = std::chrono::system_clock::now();
 		while (SDL_PollEvent(&event))
 		{
             if (event.type == SDL_KEYDOWN)
@@ -55,6 +58,12 @@ int main(int, char**)
 		}
 		scenes->Render(surface);
         SDL_UpdateWindowSurface(window.get());
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+			std::chrono::system_clock::now() - startTime);
+		const std::chrono::milliseconds minTime(1000 / 60);
+		auto waitTime = minTime - elapsed;
+		if (waitTime.count() > 0)
+			SDL_Delay(static_cast<std::uint32_t>(waitTime.count()));
     }
     SDL_Quit();
     return EXIT_SUCCESS;
